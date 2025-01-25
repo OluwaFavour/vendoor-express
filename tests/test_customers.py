ad7599f837fbf60b5f86044dde601374569ce0c4
Favour Olusayo <fsticks8187@gmail.com>
2025-01-25 18:59:59 +0100

    refactor: Rename customer-related tests to user-related tests and update endpoints

--- tests/test_customers.py
+++ tests/test_customers.py
@@ -2,147 +2,147 @@ from httpx import AsyncClient
 import pytest
 
 
-# Test the register_customer endpoint
+# Test the register_user endpoint
 @pytest.mark.anyio
-async def test_register_customer_success(client: AsyncClient):
+async def test_register_user_success(client: AsyncClient):
     response = await client.post(
-        "/customers/auth/register",
+        "/users/auth/register",
         data={
             "email": "foo@foo.com",
             "first_name": "Foo",
             "last_name": "Bar",
             "password": "pAssword123$",
             "phone": "+2348123456789",
-            "role": "customer",
+            "role": "user",
         },
     )
     assert response.status_code == 201
 
 
 @pytest.mark.anyio
-async def test_register_customer_invalid_password(client: AsyncClient):
+async def test_register_user_invalid_password(client: AsyncClient):
     response = await client.post(
-        "/customers/auth/register",
+        "/users/auth/register",
         data={
             "email": "foo@foo.com",
             "first_name": "Foo",
             "last_name": "Bar",
             "password": "password",
             "phone": "+2348123456789",
-            "role": "customer",
+            "role": "user",
         },
     )
     assert response.status_code == 422
 
 
 @pytest.mark.anyio
-async def test_register_customer_no_duplicate_email(client: AsyncClient):
+async def test_register_user_no_duplicate_email(client: AsyncClient):
     response = await client.post(
-        "/customers/auth/register",
+        "/users/auth/register",
         data={
             "email": "foo@foo.com",
             "first_name": "Foo",
             "last_name": "Bar",
             "password": "pAssword123$",
             "phone": "+2348123456789",
-            "role": "customer",
+            "role": "user",
         },
     )
     assert response.status_code == 201
 
     duplicate_response = await client.post(
-        "/customers/auth/register",
+        "/users/auth/register",
         data={
             "email": "foo@foo.com",
             "first_name": "Foo",
             "last_name": "Bar",
             "password": "pAssword123$",
             "phone": "+2348123456789",
-            "role": "customer",
+            "role": "user",
         },
     )
     assert duplicate_response.status_code == 422
 
 
 @pytest.mark.anyio
-async def test_register_customer_no_duplicate_phone(client: AsyncClient):
+async def test_register_user_no_duplicate_phone(client: AsyncClient):
     response = await client.post(
-        "/customers/auth/register",
+        "/users/auth/register",
         data={
             "email": "foo@foo.com",
             "first_name": "Foo",
             "last_name": "Bar",
             "password": "pAssword123$",
             "phone": "+2348123456789",
-            "role": "customer",
+            "role": "user",
         },
     )
     assert response.status_code == 201
 
     duplicate_response = await client.post(
-        "/customers/auth/register",
+        "/users/auth/register",
         data={
             "email": "foo@foo1.com",
             "first_name": "Foo",
             "last_name": "Bar",
             "password": "pAssword123$",
             "phone": "+2348123456789",
-            "role": "customer",
+            "role": "user",
         },
     )
 
     assert duplicate_response.status_code == 422
 
 
-# Test the login_customer endpoint
+# Test the login_user endpoint
 @pytest.mark.anyio
-async def test_login_customer_with_email(client: AsyncClient):
-    # Register a customer
+async def test_login_user_with_email(client: AsyncClient):
+    # Register a user
     register_response = await client.post(
-        "/customers/auth/register",
+        "/users/auth/register",
         data={
             "email": "foo@foo.com",
             "first_name": "Foo",
             "last_name": "Bar",
             "password": "pAssword123$",
             "phone": "+2348123456789",
-            "role": "customer",
+            "role": "user",
         },
     )
     assert register_response.status_code == 201
 
-    # Login the customer
+    # Login the user
     response = await client.post(
-        "/customers/auth/login",
+        "/users/auth/login",
         data={
             "email": "foo@foo.com",
             "password": "pAssword123$",
         },
     )
-    customer = response.json()
+    user = response.json()
     assert client.cookies["session"] is not None
     assert response.status_code == 200
 
 
 @pytest.mark.anyio
-async def test_login_customer_invalid_password(client: AsyncClient):
-    # Register a customer
+async def test_login_user_invalid_password(client: AsyncClient):
+    # Register a user
     register_response = await client.post(
-        "/customers/auth/register",
+        "/users/auth/register",
         data={
             "email": "foo@foo.com",
             "first_name": "Foo",
             "last_name": "Bar",
             "password": "pAssword123$",
             "phone": "+2348123456789",
-            "role": "customer",
+            "role": "user",
         },
     )
     assert register_response.status_code == 201
 
-    # Login the customer
+    # Login the user
     response = await client.post(
-        "/customers/auth/login",
+        "/users/auth/login",
         data={
             "email": "foo@foo.com",
             "password": "pAssword13$",
@@ -153,24 +153,24 @@ async def test_login_customer_invalid_password(client: AsyncClient):
 
 
 @pytest.mark.anyio
-async def test_logout_customer(client: AsyncClient):
-    # Register a customer
-    customer = await client.post(
-        "/customers/auth/register",
+async def test_logout_user(client: AsyncClient):
+    # Register a user
+    user = await client.post(
+        "/users/auth/register",
         data={
             "email": "foo@foo.com",
             "first_name": "Foo",
             "last_name": "Bar",
             "password": "pAssword123$",
             "phone": "+2348123456789",
-            "role": "customer",
+            "role": "user",
         },
     )
-    assert customer.status_code == 201
+    assert user.status_code == 201
 
-    # Login the customer
+    # Login the user
     login_response = await client.post(
-        "/customers/auth/login",
+        "/users/auth/login",
         data={
             "email": "foo@foo.com",
             "password": "pAssword123$",
@@ -180,7 +180,7 @@ async def test_logout_customer(client: AsyncClient):
 
     assert "session" in client.cookies
 
-    # Logout the customer
-    response = await client.post("/customers/auth/logout")
+    # Logout the user
+    response = await client.post("/users/auth/logout")
     assert response.status_code == 200
     assert "session" not in client.cookies
