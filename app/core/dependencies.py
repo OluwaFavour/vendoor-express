43a8b31a463e12a99e9d5e560f07d1204a58890b
Favour Olusayo <fsticks8187@gmail.com>
2025-01-25 18:42:42 +0100

    refactor: Update customer references to user in dependencies.py

--- app/core/dependencies.py
+++ app/core/dependencies.py
@@ -5,56 +5,56 @@ from fastapi import Request, Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.db.config import get_async_session
-from app.db.models import Customer
+from app.db.models import User
 
 
-async def get_logged_in_customer(
+async def get_logged_in_user(
     request: Request, session: Annotated[AsyncSession, Depends(get_async_session)]
-) -> Customer | None:
+) -> User | None:
     """
-    get_logged_in_customer retrieves the logged in customer from the session.
+    get_logged_in_user retrieves the logged in user from the session.
 
     Args:
         request (Request): The request object.
         session (AsyncSession): The SQLAlchemy AsyncSession.
 
     Returns:
-        Customer | None: The logged in customer, or None if not found.
+        User | None: The logged in user, or None if not found.
     """
-    customer_id = request.session.get("customer_id")
-    if not customer_id:
+    user_id = request.session.get("user_id")
+    if not user_id:
         return None
 
     try:
-        # Ensure customer_id is a valid UUID
-        customer_uuid = UUID(customer_id)
+        # Ensure user_id is a valid UUID
+        user_uuid = UUID(user_id)
     except ValueError:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Invalid session customer ID",
+            detail="Invalid session user ID",
         )
 
     try:
-        customer = await Customer.get(session, id=customer_uuid)
+        user = await User.get(session, id=user_uuid)
     except ValueError as e:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
-    return customer
+    return user
 
 
-async def get_logged_in_active_customer(
+async def get_logged_in_active_user(
     request: Request, session: Annotated[AsyncSession, Depends(get_async_session)]
-) -> Customer | None:
+) -> User | None:
     """
-    get_logged_in_active_customer retrieves the logged in active customer from the session.
+    get_logged_in_active_user retrieves the logged in active user from the session.
 
     Args:
         request (Request): The request object.
         session (AsyncSession): The SQLAlchemy AsyncSession.
 
     Returns:
-        Customer | None: The logged in active customer, or None if not found.
+        User | None: The logged in active user, or None if not found.
     """
-    customer = await get_logged_in_customer(request, session)
-    if customer and customer.is_active and not customer.is_deleted:
-        return customer
+    user = await get_logged_in_user(request, session)
+    if user and user.is_active and not user.is_deleted:
+        return user
     return None
