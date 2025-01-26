5c35f025fc72c6871801e257ebfb800aafbf9b24
Favour Olusayo <fsticks8187@gmail.com>
2025-01-26 21:33:42 +0100

    feat: Add asynchronous SMTP client for sending emails

--- app/core/dependencies.py
+++ app/core/dependencies.py
@@ -1,9 +1,11 @@
 from typing import Annotated
 from uuid import UUID
 
+import aiosmtplib
 from fastapi import Request, Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.core.config import get_settings
 from app.db.config import get_async_session
 from app.db.models import User
 
@@ -58,3 +60,61 @@ async def get_logged_in_active_user(
     if user and user.is_active and not user.is_deleted:
         return user
     return None
+
+
+async def get_async_smtp():
+    """
+    Asynchronous generator to get an SMTP client for sending emails.
+
+    This function initializes an asynchronous SMTP client using the settings
+    retrieved from the application configuration. It attempts to connect to the
+    SMTP server, start TLS, and authenticate using the provided credentials.
+    If any step fails, an appropriate HTTPException is raised.
+
+    Yields:
+        aiosmtplib.SMTP: An authenticated and connected SMTP client.
+
+    Raises:
+        HTTPException: If there is an error connecting to the SMTP server, starting TLS,
+                       authenticating, or any other SMTP-related error.
+    """
+    SETTINGS = get_settings()
+    async_smtp = aiosmtplib.SMTP(
+        hostname=SETTINGS.smtp_host,
+        port=SETTINGS.smtp_port,
+        use_tls=False,
+        start_tls=False,
+    )
+    try:
+        await async_smtp.connect()
+        await async_smtp.starttls()
+        await async_smtp.login(SETTINGS.smtp_login, SETTINGS.smtp_password)
+        yield async_smtp
+    except aiosmtplib.SMTPConnectError as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail={"message": "Could not connect to SMTP server", "error": str(e)},
+        )
+    except aiosmtplib.SMTPHeloError as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail={"message": "Could not start TLS", "error": str(e)},
+        )
+    except aiosmtplib.SMTPAuthenticationError as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail={"message": "Could not authenticate", "error": str(e)},
+        )
+    except aiosmtplib.SMTPException as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail={"message": "An error occurred", "error": str(e)},
+        )
+    finally:
+        try:
+            await async_smtp.quit()
+        except aiosmtplib.SMTPResponseException as e:
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail={"message": "An error occurred", "error": str(e)},
+            )
